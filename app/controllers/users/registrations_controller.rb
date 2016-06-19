a95456aa26639e161e38e65274f9d85b10486c80
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-19 14:35:45 -0500

    se terminan de modificar los controller para la seguridad

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+ #before_filter :configure_sign_up_params, only: [:create]
+ #before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+   #def new
+   #  super
+   #end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  #  def create
+  #    super
+  #  end
 
   # GET /resource/edit
   # def edit
@@ -18,9 +18,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  #  def update
+  #    super
+  #  end
 
   # DELETE /resource
   # def destroy
@@ -39,14 +39,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+   #def configure_sign_up_params
+     #devise_parameter_sanitizer.for(:sign_up) << :attribute
+   #end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+   #def configure_account_update_params
+     #devise_parameter_sanitizer.for(:account_update) << :attribute
+   #end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
