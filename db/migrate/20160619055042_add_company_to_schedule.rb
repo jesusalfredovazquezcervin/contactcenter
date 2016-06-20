2a1eeab63833ef29a7b78b5a1e3705da840c393f
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-19 22:55:49 -0500

    se crea un arhivo js para companies, se modifica el css para que se pueda unasr el control chosen-select, se modifican las vistas form y new para companies, se crea nuevamente la tabla companies, se modifica el esquema con la tabla, se crean dos archivos templates scaffold para generar las vistas como se debe, se elimna un archivo de migracion porque no lo uso aun

--- db/migrate/20160619055042_add_company_to_schedule.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToSchedule < ActiveRecord::Migration
-  def change
-    add_reference :schedules, :company, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619055042) do
+ActiveRecord::Schema.define(version: 20160620005722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,7 @@ ActiveRecord::Schema.define(version: 20160619055042) do
     t.time     "sundayEnd"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "company_id"
   end
-
-  add_index "schedules", ["company_id"], name: "index_schedules_on_company_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
