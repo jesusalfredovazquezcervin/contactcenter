660e113987a5b963c058dbcc44ae86d7dd4031a4
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-20 16:27:12 -0500

    se modifica la ruta del repositorio porque me marcaba error de credenciales

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'contactcenter'
-set :repo_url, 'git@github.com:jesusalfredovazquezcervin/contactcenter.git'
+set :repo_url, 'https://github.com/jesusalfredovazquezcervin/contactcenter.git'
 set :passenger_restart_with_touch, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
