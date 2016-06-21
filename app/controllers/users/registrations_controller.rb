b114ea211009b76874cdb0573a18096bd45c3105
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-21 12:14:10 -0500

    se ajustan los metodos de new, update porque el codigo estaba en el codigo fuente de devise y este no va a produccion

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
- #before_filter :configure_sign_up_params, only: [:create]
- #before_filter :configure_account_update_params, only: [:update]
+ before_filter :configure_sign_up_params, only: [:create]
+ before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-   #def new
-   #  super
-   #end
+   def new
+     build_resource({})
+     set_minimum_password_length
+     yield resource if block_given?
+     respond_with self.resource, layout: "empty"
+   end
 
   # POST /resource
   #  def create
@@ -18,9 +21,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  #  def update
-  #    super
-  #  end
+    def update
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+      prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+      resource_updated = update_resource(resource, account_update_params)
+      yield resource if block_given?
+      if resource_updated
+        if is_flashing_format?
+          flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+              :update_needs_confirmation : :updated
+          set_flash_message :notice, flash_key
+        end
+        sign_in resource_name, resource, bypass: true
+        respond_with resource, location: after_update_path_for(resource)
+      else
+        clean_up_passwords resource
+        respond_with resource
+      end
+    end
 
   # DELETE /resource
   # def destroy
@@ -39,14 +58,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   #def configure_sign_up_params
-     #devise_parameter_sanitizer.for(:sign_up) << :attribute
-   #end
+   def configure_sign_up_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
-   #def configure_account_update_params
-     #devise_parameter_sanitizer.for(:account_update) << :attribute
-   #end
+   def configure_account_update_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
