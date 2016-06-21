542590a0fca1388334481e80362ecdfcf342931e
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-20 20:49:31 -0500

    se agrega la seccion de config del envio de correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Myapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ENV['hostname'] }
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV['username'],
+      password: ENV['password']
+  }
+
 end
