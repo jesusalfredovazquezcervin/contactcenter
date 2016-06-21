7d882284e0dee4f150bf9c8b156a46aeea2fe19d
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-20 20:00:39 -0500

    se cambia la version de capistrano de 3.5.0 a 3.4.0 porque se cambio en el gemfile a esa version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.4.0'
 
 set :application, 'contactcenter'
 set :repo_url, 'https://github.com/jesusalfredovazquezcervin/contactcenter.git'
