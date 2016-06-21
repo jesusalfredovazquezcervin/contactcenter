b62648c848d1aeaa9f883671bba44b96ddb65900
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-20 20:01:45 -0500

    se vuelve a modificar porque el que esta es cargado es el 3.4.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, 'contactcenter'
 set :repo_url, 'https://github.com/jesusalfredovazquezcervin/contactcenter.git'
