fc61a096a9fea90ac05a3811efe2734d560c36c0
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-21 12:41:35 -0500

    se ajusta para que se peudan enviar correos en prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,10 @@ Myapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => ENV['hostname'] }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV['hostname'] }
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
@@ -88,5 +89,4 @@ Myapp::Application.configure do
       user_name: ENV['username'],
       password: ENV['password']
   }
-
 end
