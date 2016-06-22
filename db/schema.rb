a798226cd6792148196ba18800e9536e1f1c134d
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-22 14:03:10 -0500

    nuevo campo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622185511) do
+ActiveRecord::Schema.define(version: 20160622190243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160622185511) do
     t.integer  "contact_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "addresses", ["contact_id"], name: "index_addresses_on_contact_id", using: :btree
