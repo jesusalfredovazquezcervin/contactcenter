49a3b9aadfeb656ef74d242015b7ade7eb831a49
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-22 09:38:04 -0500

    --se agrega validacion para convertir RFC a uppcase

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ActiveRecord::Base
   has_one :schedule
+  validates :tradeName, :rfc, :employees, :comercialBusiness,
+            presence: true
+  validates :businessName,
+            presence: true,
+            uniqueness: {scope: :rfc}
+
+  validates :rfc, uniqueness:true
+
+  before_save :uppercase_rfc
+
+  def uppercase_rfc
+    rfc.upcase!
+  end
+
 end
