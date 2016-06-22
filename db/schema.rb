b7bb4606bdefd3aa02791fe61fae70987577e0f5
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-22 16:27:51 -0500

    commit inicial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622194633) do
+ActiveRecord::Schema.define(version: 20160622212500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20160622194633) do
 
   add_index "addresses", ["company_id"], name: "index_addresses_on_company_id", using: :btree
   add_index "addresses", ["state_id"], name: "index_addresses_on_state_id", using: :btree
+
+  create_table "campaigns", force: true do |t|
+    t.string   "name"
+    t.string   "campaignType"
+    t.date     "finish"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "companies", force: true do |t|
     t.string   "tradeName"
