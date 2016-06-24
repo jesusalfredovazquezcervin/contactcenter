02616c8f841a6b930ff00b5604f35749c6f64ee6
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-23 23:41:36 -0500

    se agrega metodo para mostrar la direccion de forma completa

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ActiveRecord::Base
   belongs_to :state
   belongs_to :company
   validates :name, :presence => true
+  def full
+    self.street << " " <<  self.external  <<     (self.internal.nil? ? (""):(" int " << self.internal  ) )    << ", " << self.suburb << ", " << self.state.name << " " << self.postalCode
+  end
 end
