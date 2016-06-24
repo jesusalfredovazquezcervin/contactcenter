1d8b7ab4d3d01998a2bb9c5566d7b7c04d7fbbd6
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-06-24 01:11:20 -0500

    se finaliza para presentacion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def index
-    respond_to do |format|
-      format.html { redirect_to appviews_calendar_path}
-      format.json { head :no_content }
+    if !user_signed_in?
+
+      render :layout => "empty"
     end
   end
 end
