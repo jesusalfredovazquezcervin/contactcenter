0866486560854605345dcbf96b2a77ae51ba097a
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-07-01 01:59:52 -0500

    inicial

---
+++ app/assets/javascripts/quizzes.js
@@ -0,0 +1,37 @@
+//= require iCheck/icheck.min.js
+//= require staps/jquery.steps.min.js
+//= require validate/jquery.validate.min.js
+//= require dropzone/dropzone.js
+//= require summernote/summernote.min.js
+//= require colorpicker/bootstrap-colorpicker.min.js
+//= require cropper/cropper.min.js
+//= require datapicker/bootstrap-datepicker.js
+//= require ionRangeSlider/ion.rangeSlider.min.js
+//= require jasny/jasny-bootstrap.min.js
+//= require jsKnob/jquery.knob.js
+//= require nouslider/jquery.nouislider.min.js
+//= require switchery/switchery.js
+//= require chosen/chosen.jquery.js
+//= require fullcalendar/moment.min.js
+//= require clockpicker/clockpicker.js
+//= require daterangepicker/daterangepicker.js
+//= require dataTables/jquery.dataTables.js
+//= require dataTables/dataTables.bootstrap.js
+//= require dataTables/dataTables.responsive.js
+//= require dataTables/dataTables.tableTools.min.js
+
+
+
+//= require flot/jquery.flot.js
+//= require flot/jquery.flot.tooltip.min.js
+//= require flot/jquery.flot.resize.js
+//= require flot/jquery.flot.pie.js
+//= require flot/jquery.flot.time.js
+//= require flot/jquery.flot.spline.js
+//= require sparkline/jquery.sparkline.min.js
+//= require chartjs/Chart.min.js
+//= require morris/raphael-2.1.0.min.js
+//= require morris/morris.js
+//= require rickshaw/vendor/d3.v3.js
+//= require rickshaw/rickshaw.min.js
+//= require chartist/chartist.min.js
